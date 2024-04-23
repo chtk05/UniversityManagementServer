b67edac746773ed37f61c78b85f0b27dbe203b08
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2024-04-23 14:20:26 +0700

    feat/regist-login-api

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on port ${process.env.PORT}`);
+});
