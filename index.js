5c6762bb26093b4dbd16d8a8e006b12da5902f33
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2024-04-25 02:08:36 +0700

    course-api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoute.js");
 const userRoutes = require("./routes/userRoute.js");
+const courseRoutes = require("./routes/courseRoute.js");
 const cors = require("cors");
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/", authRoutes);
 app.use("/", userRoutes);
+app.use("/", courseRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`App is listening on port ${process.env.PORT}`);
